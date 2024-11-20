aa0207c839989dbaa14446f8e2a83a462ea36cee
falconttu <k2155027@kingston.ac.uk>
2024-11-20 13:51:20 +0000

    made screens

---
+++ src/components/screens/ModuleViewScreen.js
@@ -0,0 +1,19 @@
+
+import { StyleSheet, Text } from "react-native";
+import Screen from "../layouts/Screen";
+
+export const ModuleViewScreen = () => {
+  //--Initialisations--
+  //--State--
+  //--Handlers--
+  //--View--
+  return (
+    <Screen>
+      <Text>View</Text>
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleViewScreen;
